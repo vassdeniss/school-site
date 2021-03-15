2e6716456f160675d37c55e69e23d0fa387c8459
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-03-15 16:12:20 +0200

    V1

---
+++ code.js
@@ -0,0 +1,9 @@
+function colorChange() {
+    $(".index").css({"color": "black"});
+    $(".index2").css({"color": "white"});
+}
+
+function colorNormal() {
+    $(".index").css({"color": "white"});
+    $(".index2").css({"color": "black"});
+}
