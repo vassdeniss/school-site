743120d525638c62a2423698d793ba711ddbd94b
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-03-15 21:47:41 +0200

    Improved bootstrap

--- code.js
+++ code.js
@@ -7,3 +7,19 @@ function colorNormal() {
     $(".index").css({"color": "white"});
     $(".index2").css({"color": "black"});
 }
+
+function resize() {
+    $("#one").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormal() {
+    $("#one").css({"transform": "scale(1)"})
+}
+
+function openModalOne() {
+    $("#oneImage").modal("show"); 
+}
+
+function closeModalOne() {
+    $("#oneImage").modal("hide"); 
+}
