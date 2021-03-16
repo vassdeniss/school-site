f8144e49a4e69d3b587fba5cc7ecc46196a6025a
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-03-16 13:27:49 +0200

    final release

--- code.js
+++ code.js
@@ -8,12 +8,60 @@ function colorNormal() {
     $(".index2").css({"color": "black"});
 }
 
-function resize() {
+function resizeOne() {
     $("#one").css({"transform": "scale(1.05)"})
 }
 
-function resizeNormal() {
+function resizeNormalOne() {
     $("#one").css({"transform": "scale(1)"})
+}
+
+function resizeTwo() {
+    $("#two").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalTwo() {
+    $("#two").css({"transform": "scale(1)"})
+}
+
+function resizeThree() {
+    $("#three").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalThree() {
+    $("#three").css({"transform": "scale(1)"})
+}
+
+function resizeFour() {
+    $("#four").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalFour() {
+    $("#four").css({"transform": "scale(1)"})
+}
+
+function resizeFive() {
+    $("#five").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalFive() {
+    $("#five").css({"transform": "scale(1)"})
+}
+
+function resizeSix() {
+    $("#six").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalSix() {
+    $("#six").css({"transform": "scale(1)"})
+}
+
+function resizeSeven() {
+    $("#seven").css({"transform": "scale(1.05)"})
+}
+
+function resizeNormalSeven() {
+    $("#seven").css({"transform": "scale(1)"})
 }
 
 function openModalOne() {
@@ -22,4 +70,52 @@ function openModalOne() {
 
 function closeModalOne() {
     $("#oneImage").modal("hide"); 
+}
+
+function openModalTwo() {
+    $("#twoImage").modal("show"); 
+}
+
+function closeModalTwo() {
+    $("#twoImage").modal("hide"); 
+}
+
+function openModalThree() {
+    $("#threeImage").modal("show"); 
+}
+
+function closeModalThree() {
+    $("#threeImage").modal("hide"); 
+}
+
+function openModalFour() {
+    $("#fourImage").modal("show"); 
+}
+
+function closeModalFour() {
+    $("#fourImage").modal("hide"); 
+}
+
+function openModalFive() {
+    $("#fiveImage").modal("show"); 
+}
+
+function closeModalFive() {
+    $("#fiveImage").modal("hide"); 
+}
+
+function openModalSix() {
+    $("#sixImage").modal("show"); 
+}
+
+function closeModalSix() {
+    $("#sixImage").modal("hide"); 
+}
+
+function openModalSeven() {
+    $("#sevenImage").modal("show"); 
+}
+
+function closeModalSeven() {
+    $("#sevenImage").modal("hide"); 
 }
